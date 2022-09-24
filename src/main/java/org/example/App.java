5c8c7028ac6dd9f9898d4780100732e483c3594a
Fitz <407483265@qq.com>
2022-09-24 19:38:12 +0800

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,44 @@
+package org.example;
+
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        new ZooKeeper("1", 3000, new Watcher() {
+            @Override
+            public void process(WatchedEvent event) {
+                Event.KeeperState state = event.getState();
+                Event.EventType type = event.getType();
+                switch (state) {
+                    case Unknown:
+                        break;
+                    case Disconnected:
+                        break;
+                    case NoSyncConnected:
+                        break;
+                    case SyncConnected:
+                        break;
+                    case AuthFailed:
+                        break;
+                    case ConnectedReadOnly:
+                        break;
+                    case SaslAuthenticated:
+                        break;
+                    case Expired:
+                        break;
+                    case Closed:
+                        break;
+                }
+            }
+        });
+    }
+}
